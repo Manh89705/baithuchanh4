f87d3fef71ad76efe94c809baf583fea5ddd24bf
Manh89705 <120938758+Manh89705@users.noreply.github.com>
2023-03-19 16:06:51 +0700

    Add files via upload

---
+++ Bai1.dart
@@ -0,0 +1,9 @@
+void main() {
+  // Tạo một danh sách tên
+  List<String> nameList = ["Alice", "Bob", "Charlie", "Dave"];
+
+  // In tất cả các tên trong danh sách
+  for (String name in nameList) {
+    print(name);
+  }
+}
